73ca38cd7771fce477a717405f93bbe9190d6024
John <1395088460zzy@gmail.com>
2020-05-09 11:20:44 +0800

    initial commit

---
+++ data_receiver.py
@@ -0,0 +1,23 @@
+import socket
+
+
+class Receiver:
+
+    def __init__(self):
+        super().__init__()
+        self.HOST = ''   # use '' to expose to all networks
+        self.PORT = 2055
+
+    def receive(self):
+        """Open specified port and return file-like object"""
+        
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((self.HOST, self.PORT))
+        sock.listen(0)
+        request, addr = sock.accept()
+        return request.makefile('r')
+
+r = Receiver()
+for line in r.receive():
+    print(line)
